250bf88ee3057603b56423661a8499d22c3250fc
Luciano Muñoz <luchisds@gmail.com>
2020-07-26 16:31:24 -0300

    Add URL's

--- bh/urls.py
+++ bh/urls.py
@@ -14,8 +14,57 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from website import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.index, name='home'),
+    # path('empresa/', views.company, name='company'),
+    # path('galeria/', views.gallery, name='gallery'),
+    # path('contacto/', views.contact, name='contact'),
+    # path('lluvias-historico/', views.historic_rain, name='historic_rain'),
+    # path('impuestos/', views.taxes, name='taxes'),
+    # path('unidades-de-negocio/', views.units, name='units'),
+    # path('trabaja-con-nosotros/', views.cv, name='cv'),
+    # path('cp/', views.cp, name='cp'),
+
+    # path('login/', views.auth_login, name='login'),
+    # path('login/invalido/', views.auth_login_invalid, name='login_invalid'),
+    # path('login/requerido/', views.auth_login_required, name='login_required'),
+    # path('login/cuenta-inactiva/', views.auth_login_inactive_account, name='login_inactive_account'),
+    # path('logout/', views.auth_logout, name='logout'),
+    # path('activar-cuenta/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',views.auth_activate_account, name='activate_account'),
+    # path('cuenta/cambiar-password/', views.change_password, name='change_password'),
+
+    # path('extranet/', views.extranet, name='extranet'),
+    # path('extranet/notificaciones/', views.notifications, name='notifications'),
+    # path('extranet/ctacte/pesos/<str:ctacte_type>/', views.ctacte, name='ctacte'),
+    # path('extranet/ctacte/aplicada/', views.applied, name='applied'),
+    # path('extranet/entregas/', views.deliveries, name='deliveries'),
+    # path('extranet/ventas/', views.sales, name='sales'),
+
+    # path('downloadexcel/rain/', views.downloadRainExcel, name='download_rain'),
+    # path('downloadexcel/(?P<module>[0-9A-Za-z_\-]+)/', views.downloadexcel, name='downloadexcel'),
+    # path('downloadexcel/(?P<module>[0-9A-Za-z_\-]+)(?:/(?P<type_module>[0-9A-Za-z_\-]+))/', views.downloadexcel, name='downloadexcel'),
+    # path('download/', views.downloadPDFExtranet, name='downloadPDF'),
+
+    # path('monedas/', views.get_currency, name='currency'),
+    # path('pizarras/', views.get_board, name='board'),
+    # path('lluvias/', views.get_rain, name='rain'),
+
+    # path('importar/<str:datatype>/', views.importdata, name='importdata'),
+
+    # path('400/', views.handler404, name='handler400'),
+    # path('404/', views.handler404, name='handler404'),
+    # path('500/', views.handler500, name='handler500'),
+
+    # Internazionalization
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
+
+# handler400 = 'website.views.handler404'
+# handler404 = 'website.views.handler404'
+# handler500 = 'website.views.handler500'
--- website/views.py
+++ website/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from website.models import CtaCte
+from website.models import Deliveries
+from website.models import Sales
+from website.models import SpeciesHarvest
+from website.models import Applied
+from website.models import UserInfo
+from website.models import Notifications
+from website.models import ViewedNotifications
+from website.models import Currencies
+from website.models import Board
+from website.models import TicketsAnalysis
+from website.models import City
+from website.models import Rain
+from website.models import RainDetail
+
+
+def index(request):
+    currency = Currencies.objects.order_by('-date')[:1]
+    board = Board.objects.order_by('-date')[:1]
+    rain = Rain.objects.order_by('-date')[:1]
+    rain = RainDetail.objects.filter(rain=rain).order_by('city__city')
+
+    return render(request, 'index.html', {'currency': currency, 'board': board, 'rain': rain})
