b432247cc9f3d45a9d3e3c574721abab7f10651e
luciano-im <luchisds@gmail.com>
2021-10-25 18:00:31 -0300

    ✅➕ Add model tests & mixer package

--- website/tests.py
+++ website/tests.py
@@ -1,3 +1,5 @@
+from mixer.backend.django import mixer
+
 from django.test import TestCase
 
 from django.db.models.signals import post_save
@@ -5,7 +7,7 @@ from django.db.models.signals import pre_save
 from website.signals import postSave_User, preSave_User
 
 from django.contrib.auth.models import User 
-from .models import UserInfo
+from .models import UserInfo, IncomeQuality, TicketsAnalysis, Deliveries, Sales
 
 # Model Tests
 
@@ -24,4 +26,50 @@ class UserInfoModelTest(TestCase):
         )
     
     def test_userinfo_str(self):
+        self.assertTrue(isinstance(self.user_info, UserInfo))
+        self.assertEqual(str(self.user_info), self.user_info.company_name)
         self.assertEqual(str(self.user_info), 'Test Company')
+
+
+class IncomeQualityModelTest(TestCase):
+
+    def setUp(self):
+        self.income_quality = mixer.blend(IncomeQuality, ticket='TK 0001 00090480')
+    
+    def test_incomequality_str(self):
+        self.assertTrue(isinstance(self.income_quality, IncomeQuality))
+        self.assertEqual(str(self.income_quality), self.income_quality.ticket)
+        self.assertEqual(str(self.income_quality), 'TK 0001 00090480')
+
+
+class TicketsAnalysisModelTest(TestCase):
+
+    def setUp(self):
+        self.ticket_analysis = mixer.blend(TicketsAnalysis, ticket='TK 0001 00090480')
+    
+    def test_ticketsanalysis_str(self):
+        self.assertTrue(isinstance(self.ticket_analysis, TicketsAnalysis))
+        self.assertEqual(str(self.ticket_analysis), self.ticket_analysis.ticket)
+        self.assertEqual(str(self.ticket_analysis), 'TK 0001 00090480')
+
+
+class DeliveriesModelTest(TestCase):
+
+    def setUp(self):
+        self.ticket = mixer.blend(Deliveries, voucher='TK 0021 00042866')
+    
+    def test_deliveries_str(self):
+        self.assertTrue(isinstance(self.ticket, Deliveries))
+        self.assertEqual(str(self.ticket), self.ticket.voucher)
+        self.assertEqual(str(self.ticket), 'TK 0021 00042866')
+
+
+class SalesModelTest(TestCase):
+
+    def setUp(self):
+        self.sale = mixer.blend(Sales, voucher='VT 0001 00022925')
+    
+    def test_deliveries_str(self):
+        self.assertTrue(isinstance(self.sale, Sales))
+        self.assertEqual(str(self.sale), self.sale.voucher)
+        self.assertEqual(str(self.sale), 'VT 0001 00022925')
