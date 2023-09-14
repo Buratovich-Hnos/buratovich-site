20857a8e6513e52a89ff58bc7987c8db7947122c
Luciano Muñoz <luchisds@gmail.com>
2023-09-14 13:45:46 -0300

    ♻️ Move signal import statement from website app to extranet app

--- extranet/apps.py
+++ extranet/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ExtranetConfig(AppConfig):
     name = 'extranet'
+
+    # Connecting signals
+    def ready(self):
+        import extranet.signals
--- website/apps.py
+++ website/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class WebsiteConfig(AppConfig):
     name = 'website'
-
-    # Connecting signals
-    def ready(self):
-        import extranet.signals
